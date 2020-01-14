5b03a0504e181fb348f7a904e868cba17206c49d
KavinduLana <kavindu199805@gmail.com>
2020-01-14 14:40:07 +0530

    Modified the status

--- GUI/src/app/app.module.ts
+++ GUI/src/app/app.module.ts
@@ -8,14 +8,11 @@ import { AddComponent } from './add/add.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SuccessComponent } from './success/success.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-<<<<<<< HEAD
 import { SearchComponent } from './search/search.component';
 import { StatusComponent } from './status/status.component';
 
-=======
 import { HttpClientModule } from '@angular/common/http';
 import { DatePipe } from '@angular/common';
->>>>>>> d6a1220ae436c8460bf0e538c5d6da9d6fd1285f
 
 @NgModule({
   declarations: [
